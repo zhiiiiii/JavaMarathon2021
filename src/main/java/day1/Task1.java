3b56f77d2a83d5bd9647736e4fb36227a2ef476c
Nurila Zharkynbek <zharkynbeknurila@gmail.com>
2021-02-23 17:27:44 +0600

    day1

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int n = 0;
+        while(n<10){
+            System.out.print("JAVA" + " ");
+            n++;
+        }
     }
 }
--- src/main/java/day1/Task4.java
+++ src/main/java/day1/Task4.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task4 {
     public static void main(String[] args) {
-
+       int n = 1980;
+       while(n<=2020){
+           System.out.println("Olympiad " + n + " year");
+           n+=4;
+       }
     }
 }
