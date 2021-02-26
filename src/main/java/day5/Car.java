fbf9b02a8fe14c6b813232e89fd39900f707e2f2
Nurila Zharkynbek <zharkynbeknurila@gmail.com>
2021-02-26 22:53:31 +0600

    day5

---
+++ src/main/java/day5/Car.java
@@ -0,0 +1,31 @@
+package day5;
+
+public class Car {
+    int yearOfRelease;
+    String color;
+    String model;
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
